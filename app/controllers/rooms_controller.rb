3f05edb92a75280c8dcc65bdc38db97365cab0f1
Krzysztof Kuchna <kkuchna@gmail.com>
2019-07-31 15:55:37 +0200

    Fix style

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,7 +1,7 @@
+# frozen_string_literal: true
+
 class RoomsController < ApplicationController
-  
-    def show
-      @messages = Room.first.all
-    end
+  def show
+    @messages = Room.first.all
   end
-  
+end
